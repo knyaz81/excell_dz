3d65fd3b077306398b9b7e99d1a4c0d1741ea7e9
Alexandr Knyazkov <a.knz1981@gmail.com>
2020-06-06 20:40:17 +0300

    T-001: Added cli argument parser

--- manage.py
+++ manage.py
@@ -3,7 +3,12 @@ import sys
 
 def main():
     print('Python v', sys.version, sep='')
-    print('Excell parser application.\nStatus: in work...')
+    print('Excell parser application.\n')
+
+    from src.core.args import args_parser
+    ARGUMENTS, _ = args_parser.parse_known_args()
+
+    args_parser.print_help()
 
 if __name__ == '__main__':
     main()
---
+++ src/core/args.py
@@ -0,0 +1,16 @@
+import argparse
+
+args_parser =  argparse.ArgumentParser()
+
+args_parser.add_argument(
+    'action', type=str, nargs='?', default='help',
+    help=(
+        'Action to execute, required positional argument. Use: '
+        'generate, initdb, checkdb'
+    )
+)
+
+args_parser.add_argument(
+    '-f', '--file', type=str, default='temp_dz.xlsx',
+    help='full excell filename, e.g. "-f example.xslx", default "temp_dz.xlsx"'
+)
